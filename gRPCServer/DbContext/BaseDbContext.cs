8c3f4345583d7e29be337f11d146f9fa42820a59
RezanSoylemez <rezan.sm@gmail.com>
2023-11-13 17:17:27 +0300

    FirstBranch

---
+++ gRPCServer/DbContext/BaseDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+public class BaseDbContext : DbContext
+{
+    public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+ 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Veritabanı modelini konfigure etme işlemleri buraya eklenebilir.
+    }
+}
